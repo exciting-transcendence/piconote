b8b9ef8e05ac06606ef852cf93f33af4dc7ea86f
scarf <greenscarf005@gmail.com>
2022-07-22 20:05:58 +0900

    style: format

--- frontend/src/Sidebar.tsx
+++ frontend/src/Sidebar.tsx
@@ -8,40 +8,63 @@ const shorten = (str: string, maxLength = 100) => {
   return str
 }
 
-const NotePreview = ({ note, onDeleteNote }:
-  { note: Note, onDeleteNote: (id: string) => void }) => {
+const NotePreview = ({
+  note,
+  onDeleteNote,
+  setActiveNote,
+  activeNote,
+}: {
+  note: Note
+  onDeleteNote: (id: string) => void
+  activeNote: string | null
+  setActiveNote: any
+}) => {
   return (
-    <div className='app-sidebar-note'>
-      <div className='sidebar-note-title'>
+    <div
+      className={`app-sidebar-note ${note.id === activeNote && 'active'}`}
+      onClick={() => setActiveNote(note.id)}>
+      <div className="sidebar-note-title">
         <strong>{note.title}</strong>
         <button onClick={() => onDeleteNote(note.id)}>Delete</button>
       </div>
 
       <p>{note.body ? shorten(note.body) : 'empty note'}</p>
-      <small className='note-meta'>
+      <small className="note-meta">
         Last Modified {new Date(note.lastModified).toISOString()}
       </small>
     </div>
   )
 }
 
-const Sidebar = (
-  { notes, onAddNote, onDeleteNote }:
-    { notes: Note[], onAddNote: () => void, onDeleteNote: (id: string) => void }) => {
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}: {
+  notes: Note[]
+  onAddNote: () => void
+  onDeleteNote: (id: string) => void
+  activeNote: string | null
+  setActiveNote: any
+}) => {
   return (
-    <div className='app-sidebar'>
-      <div className='app-sidebar-header'>
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
         <h1>Notes</h1>
         <button onClick={onAddNote}>Add</button>
       </div>
-      <div className='app-sidebar-notes'>
+      <div className="app-sidebar-notes">
         {notes.map(note => (
           <NotePreview
             note={note}
             key={note.id}
             onDeleteNote={onDeleteNote}
-          />)
-        )}
+            activeNote={activeNote}
+            setActiveNote={setActiveNote}
+          />
+        ))}
       </div>
     </div>
   )
